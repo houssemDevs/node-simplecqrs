e7f8fefbedfa5aea796b70f28389c9d9b8a513b0
Houssem Ghiat <h.ghiat@ctcdre.loc>
2019-04-03 14:32:22 +0100

    default implementation for TdsDataMapper

--- src/impl/sql/tedious/tdsdatamapper.ts
+++ src/impl/sql/tedious/tdsdatamapper.ts
@@ -3,3 +3,12 @@ import { IDateMapper } from '../../../common/datamapper';
 
 export interface ITdsDataMapper<TEntity>
   extends IDateMapper<ColumnValue[], TEntity> {}
+
+export class TdsDataMapper implements ITdsDataMapper<{}> {
+  public toDomain(s: ColumnValue[]): {} {
+    return s.reduce((e, c) => ({ ...e, [c.metadata.colName]: c.value }), {});
+  }
+  public toStore(d: {}): ColumnValue[] {
+    throw new Error('Method not implemented.');
+  }
+}
