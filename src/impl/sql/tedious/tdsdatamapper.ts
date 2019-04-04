7fbca7af42797d16f5103ba4ecc472f726ffca42
Houssem GHIAT <ghiat.houssem@gmail.com>
2019-04-04 19:50:58 +0200

    make tdsdatamapper abstract class.

--- src/impl/sql/tedious/tdsdatamapper.ts
+++ src/impl/sql/tedious/tdsdatamapper.ts
@@ -4,11 +4,9 @@ import { IDateMapper } from '../../../common/datamapper';
 export interface ITdsDataMapper<TEntity>
   extends IDateMapper<ColumnValue[], TEntity> {}
 
-export class TdsDataMapper implements ITdsDataMapper<{}> {
+export abstract class TdsDataMapper implements ITdsDataMapper<{}> {
   public toDomain(s: ColumnValue[]): {} {
     return s.reduce((e, c) => ({ ...e, [c.metadata.colName]: c.value }), {});
   }
-  public toStore(d: {}): ColumnValue[] {
-    throw new Error('Method not implemented.');
-  }
+  public abstract toStore(d: {}): ColumnValue[];
 }
