cb92a3082aab9fcf474306f1af6ce8ffc1f048db
Houssem Ghiat <h.ghiat@ctcdre.loc>
2019-04-10 15:57:06 +0200

    add filter and sort criteria for symentic .

--- src/read/criteria.ts
+++ src/read/criteria.ts
@@ -1,3 +1,7 @@
 export interface ICriteria<TExpression> {
   toExpression(): TExpression;
 }
+
+export interface IFilterCriteria<TExpression> extends ICriteria<TExpression> {}
+
+export interface ISortCriteria<TExpression> extends ICriteria<TExpression> {}
