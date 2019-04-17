a14e1dd7bbea863cdf64ab00ff340e6cec50ebbe
Houssem Ghiat <h.ghiat@ctcdre.loc>
2019-04-17 12:18:34 +0200

    drop queryobject impl.

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 export { IDateMapper } from './common/datamapper';
 
 export { ICriteria, IFilterCriteria, ISortCriteria } from './read/criteria';
-export { IQuery, Query } from './read/queryobject';
+export { IQuery } from './read/queryobject';
 export { IQueryHandler } from './read/queryhandler';
 export { IQueryDispatcher } from './read/querydispatcher';
 
--- src/read/queryobject.ts
+++ src/read/queryobject.ts
@@ -1,27 +1,10 @@
-export interface IQuery<TExpression, TCriteria> {
-  addCriteria(c: TCriteria): IQuery<TExpression, TCriteria>;
-  beginNewGroup(): IQuery<TExpression, TCriteria>;
+export interface IQuery<TExpression, TFilterCriteria, TSortCriteria> {
+  addFilterCriteria(
+    c: TFilterCriteria,
+  ): IQuery<TExpression, TFilterCriteria, TSortCriteria>;
+  addSortCriteria(
+    c: TSortCriteria,
+  ): IQuery<TExpression, TFilterCriteria, TSortCriteria>;
+  beginNewFilterGroup(): IQuery<TExpression, TFilterCriteria, TSortCriteria>;
   toExpression(): TExpression;
-}
-
-export abstract class Query<TExpression, TCriteria>
-  implements IQuery<TExpression, TCriteria> {
-  protected criteriaGroups: TCriteria[][];
-  protected currentCriteriaGroup: TCriteria[];
-  constructor() {
-    this.criteriaGroups = [];
-    this.currentCriteriaGroup = [];
-  }
-  public addCriteria(c: TCriteria): IQuery<TExpression, TCriteria> {
-    this.currentCriteriaGroup.push(c);
-    return this;
-  }
-  public beginNewGroup(): IQuery<TExpression, TCriteria> {
-    if (this.currentCriteriaGroup.length > 0) {
-      this.criteriaGroups.push(this.currentCriteriaGroup);
-      this.currentCriteriaGroup = [];
-    }
-    return this;
-  }
-  public abstract toExpression(): TExpression;
 }
