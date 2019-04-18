e15623cfd5b86ec6afe3a39a3d491511536adbfc
Houssem Ghiat <h.ghiat@ctcdre.loc>
2019-04-18 15:38:28 +0200

    query, command dispatcher implementation for inversify.

--- src/ioc/inversify/commanddispatcher.ts
+++ src/ioc/inversify/commanddispatcher.ts
@@ -1,32 +1,37 @@
 import { Container, decorate, injectable } from 'inversify';
 
 import { ICommandDispatcher } from '../../write/commanddispatcher';
-import { ICommandHandler } from '../../write/commandhandler';
 import { ICommand } from '../../write/commandobject';
-import { METADATA_KEYS } from '../constants';
-import { CommandMetadata } from '../types';
+import { getCommandsMetadata, getObjectName } from '../utils';
 import { TYPES } from './constants';
+import { getCHFromContainer } from './utils';
 
 export class InvesrifyCommandDispatcher implements ICommandDispatcher {
   constructor(private container: Container) {
-    const commandsMetadata: CommandMetadata[] =
-      Reflect.getMetadata(METADATA_KEYS.commands, Reflect) || [];
+    const commandsMetadata = getCommandsMetadata();
+
+    const alreadyDecoratedHandlers: Set<any> = new Set();
+
     commandsMetadata.forEach((cm) => {
-      decorate(injectable(), cm.handler);
+      if (!alreadyDecoratedHandlers.has(cm.handler)) {
+        decorate(injectable(), cm.handler);
+        alreadyDecoratedHandlers.add(cm.handler);
+      }
+
       this.container
         .bind(TYPES.command)
         .to(cm.handler)
         .whenTargetNamed(cm.name);
     });
   }
+
   public dispatch(c: ICommand): Promise<boolean> {
-    const handler: ICommandHandler = this.container.getNamed(
-      TYPES.command,
-      Object.getPrototypeOf(c).constructor.name,
-    );
-    if (handler) {
-      return handler.exec(c);
+    const h = getCHFromContainer(this.container, getObjectName(c));
+
+    if (h) {
+      return h.exec(c);
     }
+
     throw new Error(`No command handler fro ${c}`);
   }
 }
--- src/ioc/inversify/querydispatcher.ts
+++ src/ioc/inversify/querydispatcher.ts
@@ -3,42 +3,47 @@ import { Readable } from 'stream';
 import { Container, decorate, injectable } from 'inversify';
 
 import { IQueryDispatcher } from '../../read/querydispatcher';
-import { IQueryHandler } from '../../read/queryhandler';
 import { IQuery } from '../../read/queryobject';
-import { METADATA_KEYS } from '../constants';
-import { QueryMetadata } from '../types';
+import { getObjectName, getQueriesMetadata } from '../utils';
 import { TYPES } from './constants';
+import { getQHFromContainer } from './utils';
 
 export class InversifyQueryDispatcher implements IQueryDispatcher {
   constructor(private container: Container) {
-    const queriesMetadata: QueryMetadata[] =
-      Reflect.getMetadata(METADATA_KEYS.queries, Reflect) || [];
+    const queriesMetadata = getQueriesMetadata();
+
+    const alreadyDecoratedHandlers: Set<any> = new Set();
+
     queriesMetadata.forEach((qm) => {
-      decorate(injectable(), qm.handler);
+      if (!alreadyDecoratedHandlers.has(qm.handler)) {
+        decorate(injectable(), qm.handler);
+        alreadyDecoratedHandlers.add(qm.handler);
+      }
+
       this.container
         .bind(TYPES.query)
         .to(qm.handler)
         .whenTargetNamed(qm.name);
     });
   }
+
   public dispatch<TEntity>(q: IQuery): Promise<TEntity[]> {
-    const handler: IQueryHandler<TEntity> = this.container.getNamed(
-      TYPES.query,
-      Object.getPrototypeOf(q).constructor.name,
-    );
-    if (handler) {
-      return handler.get(q);
+    const h = getQHFromContainer<TEntity>(this.container, getObjectName(q));
+
+    if (h) {
+      return h.get(q);
     }
+
     throw new Error(`no handler for ${q}`);
   }
+
   public dispatchStream<TEntity>(q: IQuery): Readable {
-    const handler: IQueryHandler<TEntity> = this.container.getNamed(
-      TYPES.query,
-      Object.getPrototypeOf(q).constructor.name,
-    );
-    if (handler) {
-      return handler.getStream(q);
+    const h = getQHFromContainer<TEntity>(this.container, getObjectName(q));
+
+    if (h) {
+      return h.getStream(q);
     }
+
     throw new Error(`no handler for ${q}`);
   }
 }
