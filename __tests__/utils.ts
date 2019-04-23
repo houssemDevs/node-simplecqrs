f90e1c20678bf73868ce2d59aaae23da8b1ba264
Houssem GHIAT <ghiat.houssem@gmail.com>
2019-04-23 21:33:11 +0100

    testing utils functions.

---
+++ __tests__/utils.ts
@@ -0,0 +1,74 @@
+import 'reflect-metadata';
+
+import {} from '../src';
+import { command, commands, queries, query } from '../src/ioc';
+import {
+  getCommandId,
+  getCommandMetadata,
+  getCommandsMetadata,
+  getQueriesMetadata,
+  getQueryId,
+  getQueryMetadata,
+} from '../src/ioc/utils';
+
+class Query {}
+class Command {}
+
+class QueryHandler {}
+class CommandHandler {}
+
+Reflect.decorate([query], Query);
+Reflect.decorate([command], Command);
+
+Reflect.decorate([queries(Query)], QueryHandler);
+Reflect.decorate([commands(Command)], CommandHandler);
+
+describe('utils', () => {
+  describe('getQueryMetadata', () => {
+    it('should return query metadata given constructor function', () => {
+      expect(getQueryMetadata(Query)).toBeDefined();
+      expect(getQueryMetadata(Query).id).toBeDefined();
+    });
+  });
+
+  describe('getQueryId', () => {
+    it('should return query id from query metadata', () => {
+      expect(getQueryId(Query)).toBeDefined();
+      expect(getQueryId(Query)).toEqual(getQueryMetadata(Query).id);
+    });
+  });
+
+  describe('getCommandMetadata', () => {
+    it('should return command metadata given constructor function', () => {
+      expect(getCommandMetadata(Command)).toBeDefined();
+      expect(getCommandMetadata(Command).id).toBeDefined();
+    });
+  });
+
+  describe('getCommandId', () => {
+    it('should return command id from command metadata', () => {
+      expect(getCommandId(Command)).toBeDefined();
+      expect(getCommandId(Command)).toEqual(getCommandMetadata(Command).id);
+    });
+  });
+
+  describe('getCommandsMetadata', () => {
+    it('should return commands metadata from Reflect global object', () => {
+      const commandsMetadata = getCommandsMetadata();
+
+      expect(commandsMetadata).toBeDefined();
+      expect(commandsMetadata.get(getCommandId(Command))).toBeDefined();
+      expect(commandsMetadata.get(getCommandId(Command)).handler).toBe(CommandHandler);
+    });
+  });
+
+  describe('getQueriesMetadata', () => {
+    it('should return queries metadata from Reflect global object', () => {
+      const queriesMetadata = getQueriesMetadata();
+
+      expect(queriesMetadata).toBeDefined();
+      expect(queriesMetadata.get(getQueryId(Query))).toBeDefined();
+      expect(queriesMetadata.get(getQueryId(Query)).handler).toBe(QueryHandler);
+    });
+  });
+});
