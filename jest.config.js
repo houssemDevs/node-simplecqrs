8cb28add499a0a293051aa4f3196881aafaa5d39
Houssem GHIAT <ghiat.houssem@gmail.com>
2019-04-23 21:33:02 +0100

    ignore TS2532 undefined checks compiler diagnostics.

--- jest.config.js
+++ jest.config.js
@@ -2,4 +2,11 @@ module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
   verbose: true,
+  globals: {
+    'ts-jest': {
+      diagnostics: {
+        ignoreCodes: [6059, 18002, 18003, 2532],
+      },
+    },
+  },
 };
